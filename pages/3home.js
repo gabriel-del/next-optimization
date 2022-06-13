f0df4edba20c621518ab645b5c9d9b8d41ddf856
Gabriel Del <gabrieldelc.barros@gmail.com>
2022-06-13 09:38:04 -0300

    2022-06-13=09:38:04

--- pages/3home.js
+++ pages/3home.js
@@ -6,6 +6,10 @@ function Home () {
     <Link href="/3sobre">
       <a>Acessa página sobre</a>
     </Link>
+    <br/>
+    <a>No ambiente de produção. Carrega a página seguinte só de passar o mouse por cima do link</a>
+    <br/>
+    <a>Mantém modificações no html, mesmo mudando de página</a>
   </div>)
 }
 
---
+++ pages/4tempo.js
@@ -0,0 +1,13 @@
+function Tempo(){
+  const dynamicDate = new Date();
+  const dynamicDateString = dynamicDate.toGMTString();
+
+  return (
+    <div>
+      {dynamicDateString} (dinâmico)
+    </div>
+  )
+
+}
+
+export default Tempo;
